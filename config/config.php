0fb0589a4ee9b68623184b901cbbc68625ef4fa5
ALEXANDER TORRES HERNANDEZ <114458694+alexkun90@users.noreply.github.com>
2023-12-11 00:10:59 -0600

    Parte1
    parte 1

---
+++ config/config.php
@@ -0,0 +1,5 @@
+<?php 
+    define('ADMIN', 'admin');
+    define('RUTA_PRINCIPAL', 'http://localhost/HotelProyect/');
+    define('RUTA_ADMIN', RUTA_PRINCIPAL . ADMIN . '/');
+?>
---
+++ controllers/principal/Blog.php
@@ -0,0 +1,11 @@
+<?php 
+
+    class Blog{
+        public function index(){
+            echo 'mensaje desde el controlador';
+        }
+        public function listar(){
+            echo 'mensaje vamos a listar los resultados';
+        }
+    }
+?>
